65e499330e4fd1cacd467959494b0a9935b51ec2
AbdurCodes <124054847+AbdurCodes@users.noreply.github.com>
2024-03-27 17:27:34 +0500

    clean the code

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
 let container = document.querySelector('.container');
-// let div;
 
-let gridSize = 23;
+let gridSize = 16;
 
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
         const div = document.createElement('div');
         const cellWidth = (100 / gridSize) + "%";
-        div.style.cssText = `flex: 1 1 ${cellWidth};  border: 1px solid red;`;
+        div.style.cssText = `flex: 1 1 ${cellWidth}; border: 1px solid red;`;
         // div.textContent = j + 1;
         container.appendChild(div);
     }
 }
-
-console.log('Abdur');
-
-// let container = document.querySelector('.container');
-// let gridSize = 13; // Change this value for different grid sizes
-
-// for (let i = 0; i < gridSize; i++) {
-//   for (let j = 0; j < gridSize; j++) {
-//     const cell = document.createElement('div');
-//     const cellWidth = Math.floor(100 / gridSize) + '%'; // Calculate width with potential rounding
-//     cell.style.cssText = `flex: 1 1 ${cellWidth}; border: 1px solid red;`;
-//     // cell.textContent = (i * gridSize) + j + 1; // Combine row and column numbers
-//     container.appendChild(cell);
-//   }
-// }
